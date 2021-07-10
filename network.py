abc5794e7f93cfc83521534b805cd83167e23f1c
Noor <noorulan29@gmail.com>
2021-07-11 02:45:39 +0500

    no message

--- network.py
+++ network.py
@@ -3,9 +3,9 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class NeuralNetwork(nn.Module):
-    def __init__(self,num_classes):
-        super(NeuralNetwork, self).__init__()
+class TargetNeuralNetwork(nn.Module):
+    def __init__(self):
+        super(TargetNeuralNetwork, self).__init__()
         self.conv_1 = nn.Conv2d(3, 64, kernel_size=(3, 3), padding=(1, 1))
         self.conv_2 = nn.Conv2d(64, 512, kernel_size=(3, 3), padding=(1, 1))
         self.conv_3 = nn.Conv2d(512, 512, kernel_size=(3, 3), padding=(1, 1))
@@ -35,4 +35,23 @@ class NeuralNetwork(nn.Module):
         print(x.size())
         x_ = self.fc2(x)
         print(x_.size())
-        return x_.float(),x
+        return x_,x
+
+
+class AttackNeuralNetwork(nn.Module):
+    def __init__(self):
+        super(AttackNeuralNetwork, self).__init__()
+
+        self.fc1 = nn.Linear(320000, 5000)
+        self.fc2 = nn.Linear(5000, 128)      
+        self.fc3 = nn.Linear(128, 2)
+
+    def forward(self, x):
+        print(x.size())
+        x = F.relu(self.fc1(x))
+        print(x.size())
+        x = F.relu(self.fc2(x))
+        print(x.size())        
+        x = self.fc3(x)
+        print(x.size())
+        return x
